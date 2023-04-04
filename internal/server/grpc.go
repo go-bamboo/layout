6c65d36a20128d886365f1b9f7a4210dcf480424
songhao <emberfarkas@hotmail.com>
2023-04-04 14:56:06 +0800

    Update grpc.go

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -3,11 +3,10 @@ package server
 import (
 	"github.com/go-bamboo/layout/internal/conf"
 	"github.com/go-bamboo/pkg/rpc"
-	"github.com/go-kratos/kratos/v2/transport/grpc"
 )
 
 // NewGRPCServer new a gRPC server.
-func NewGRPCServer(c *conf.Server) *grpc.Server {
+func NewGRPCServer(c *conf.Server) *rpc.Server {
 	srv := rpc.NewServer(c.Grpc)
 	return srv
 }
