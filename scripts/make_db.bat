46d565ae003a2539268041e9e14c2e011512f1da
songhao <emberfarkas@hotmail.com>
2023-07-25 10:56:44 +0800

    Update make_db.bat

--- scripts/make_db.bat
+++ scripts/make_db.bat
@@ -1,4 +1,4 @@
-mysql -uroot -p123456 -h127.0.0.1 -e "drop database if exists ncsc_ex"
-mysql -uroot -p123456 -h127.0.0.1 -e "CREATE DATABASE ncsc_ex DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci"
+mysql -uroot -p123456 -h127.0.0.1 -e "drop database if exists layout"
+mysql -uroot -p123456 -h127.0.0.1 -e "CREATE DATABASE layout DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci"
 
 pause
