99f5b83e8371de01f8f03d302da85ba01cd9d881
emberfarkas <emberfarkas@hotmail.com>
2024-09-03 13:43:43 +0800

    feat: app

--- cmd/server/di/app.go
+++ cmd/server/di/app.go
@@ -2,12 +2,12 @@ package di
 
 import (
 	"github.com/go-bamboo/layout/internal/conf"
+	"github.com/go-bamboo/pkg/kratos"
 	"github.com/go-bamboo/pkg/log"
 	"github.com/go-bamboo/pkg/log/core"
 	"github.com/go-bamboo/pkg/registry"
 	"github.com/go-bamboo/pkg/rest"
 	"github.com/go-bamboo/pkg/rpc"
-	"github.com/go-kratos/kratos/v2"
 )
 
 func newApp(id string, srv *conf.Service, logger core.Logger, gs *rpc.Server, hs *rest.Server, r registry.Registrar) *kratos.App {
--- go.mod
+++ go.mod
@@ -116,6 +116,7 @@ require (
 	go.opentelemetry.io/otel/sdk/metric v1.24.0 // indirect
 	go.opentelemetry.io/otel/trace v1.24.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/automaxprocs v1.5.3 // indirect
 	go.uber.org/goleak v1.2.1 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	go.uber.org/zap v1.24.0 // indirect
