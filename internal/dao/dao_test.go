fd94a5ed26d0bd7936e943195210bd11fe148c68
emberfarkas <emberfarkas@hotmail.com>
2024-09-03 11:01:13 +0800

    Update dao_test.go

--- internal/dao/dao_test.go
+++ internal/dao/dao_test.go
@@ -1,28 +1,18 @@
 package dao
 
-import (
-	"context"
-	"flag"
-	"os"
-	"testing"
-
-	"github.com/go-bamboo/layout/internal/conf"
-	"github.com/go-bamboo/pkg/config"
-)
-
-var d *dao
-var ctx = context.Background()
-
-func TestMain(m *testing.M) {
-	flag.Set("conf", "file:///../../configs/test.yaml")
-	var err error
-	var bc conf.Bootstrap
-	config.LoadFlag(&bc)
-	var cf func()
-	if d, cf, err = newDao(bc.Data, nil); err != nil {
-		panic(err)
-	}
-	ret := m.Run()
-	cf()
-	os.Exit(ret)
-}
+//var d *dao
+//var ctx = context.Background()
+//
+//func TestMain(m *testing.M) {
+//	flag.Set("conf", "file:///../../configs/test.yaml")
+//	var err error
+//	var bc conf.Bootstrap
+//	config.LoadFlag(&bc)
+//	var cf func()
+//	if d, cf, err = newDao(bc.Data, nil); err != nil {
+//		panic(err)
+//	}
+//	ret := m.Run()
+//	cf()
+//	os.Exit(ret)
+//}
